0d737ca18b911c24d672b693bac5307ae5b6d826
g0 <13pft13@gmail.com>
2020-01-12 16:20:13 +0900

    Refactor using sort()

--- programmers_Lv1_42576.cpp
+++ programmers_Lv1_42576.cpp
@@ -1,21 +1,16 @@
 //https://programmers.co.kr/learn/courses/30/lessons/42576
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <map>
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-	map<string, int> participantFlag;
-	map<string, int> completionFlag;
-	for (auto const& x : participant) { participantFlag[x] = 0; }
-	for (auto const& x : completion) { completionFlag[x] = 0; }
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
 
-	for (size_t i = 0; i < participant.size(); i++)
-		participantFlag[participant[i]]++;
+	for (size_t i = 0; i < participant.size();i++) {
+		if (participant[i] != completion[i]) return participant[i];
+	}
 
-	for (size_t i = 0; i < completion.size(); i++) 
-		completionFlag[completion[i]]++;
-
-	for (const auto& p : participantFlag)
-		if ((completionFlag.count(p.first) == 0) || (p.second != completionFlag[p.first])) return p.first;
+	return participant.back();
 }
