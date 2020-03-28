f13edecaecb3fc395490dc8ec1db5185a6e739d0
wooooooood <40855076+wooooooood@users.noreply.github.com>
2020-03-28 22:55:41 +0900

    refactor

--- Programmers/Lv2_42577.cpp
+++ Programmers/Lv2_42577.cpp
@@ -1,20 +1,15 @@
 //https://programmers.co.kr/learn/courses/30/lessons/42577
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    for (size_t i = 0; i<phone_book.size();i++){
-        for (size_t j = 0; j<phone_book.size(); j++){
-            if (i==j) break;
-
-            string longString = phone_book[i].length()>phone_book[j].length()? phone_book[i]:phone_book[j];
-            string shortString = phone_book[i].length()<=phone_book[j].length()? phone_book[i]:phone_book[j];
-            
-            if (longString.substr(0,shortString.length()) == shortString){
-                return false;    
-            }
-        }
+    sort(phone_book.begin(), phone_book.end());
+    
+    for (size_t i = 0; i<phone_book.size()-1;i++){
+            if (phone_book[i] == phone_book[i+1].substr(0, phone_book[i].length()))
+                return false;
     }
     
     return true;
