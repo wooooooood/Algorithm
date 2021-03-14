04098590928b6f762a3359caf7a57d3d75238e94
wooooooood <40855076+wooooooood@users.noreply.github.com>
2021-03-14 18:06:58 +0900

    fix typo

--- Programmers/Lv1_70128.js
+++ Programmers/Lv1_70128.js
@@ -8,7 +8,7 @@ function solution(a, b) {
 }
 
 /*
-처음에 reduce를 사용하려 했지만 실패함. 다른 사람의 풀이 첨부.
+처음에 reduce만 사용하려 했지만 실패함. 다른 사람의 풀이 첨부.
 function solution(a, b) {
     return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
 }
