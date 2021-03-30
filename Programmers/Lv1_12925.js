ca0a703792a542157965f181e917c7d64d3bf49e
wooooooood <40855076+wooooooood@users.noreply.github.com>
2021-03-30 23:07:05 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/Lv1_12925.js
@@ -0,0 +1,11 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12925
+function solution(s) {
+  return Number(s,10);
+}
+
+/*
+다른 사람의 풀이
+1. return str/1
+2. return +str
+3. return Number(str)
+*/
